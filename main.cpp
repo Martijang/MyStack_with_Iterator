f19e8ee0bb3fd424ac19536abd836e7f83cb6cb4
Martijang <seungwoogo1123@gmail.com>
2025-09-25 08:18:57 +0800

    main file
    Example usages.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Stack.hpp"
+
+int main(){
+    Stack<int> st;
+
+    std::cout << "SIZE: " << st.get_size() << "\n";
+    for (int i = 0; i < 14; i++)
+        st.push(i);
+    
+    //Will be printing from 0-13 not 13-0
+    for (int &i: st)
+        std::cout << "POP! " << i << std::endl;
+    
+    std::cout << std::endl;
+
+    //To print out just like stack
+    for (int i = 0; i < st.get_size(); i++)
+        std::cout << "POP! " << st.pop() << std::endl;
+
+    std::cout << "SIZE: " <<st.get_size() << std::endl;
+    st.resize(30);
+    std::cout << "SIZE: "<<st.get_size() << std::endl;
+    
+    return 0;
+}
